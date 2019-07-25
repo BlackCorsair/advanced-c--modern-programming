351bb714a06551fb98e70482656617099a448c56
Jorge Hevia <jmhev@outlook.com>
2019-07-25 20:59:00 +0200

    fixed_vector exercise (1)

---
+++ src/fixed_vector.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+template <class T, int N>
+class fixed_vector
+{
+public:
+    const T * begin() const {return array;}
+    const T * end() const {return array + N;}
+
+    fixed_vector(std::initializer_list<T> list) {
+        std::copy(list.begin(), list.end(), array);
+    }
+    fixed_vector(void){}
+private:
+    T array[N];
+};
+
+// have to run on this
+template <typename T, int N>
+void print(const fixed_vector<T,N> & v) {
+    for (auto const & x: v) { 
+        std::cout << x << " ";
+    }
+    std::cout << "\n";
+}
+
+int main() { 
+    fixed_vector<double,3> v; 
+    print(v);
+    fixed_vector<int,4> w{1,2,3,4}; 
+    print(w);
+}
