13d54ee40ee5031e6653ed301dd19f5c2ccf840a
Jorge Hevia <jmhev@outlook.com>
2019-07-26 15:50:25 +0200

    Compliance with cpplint

--- src/fixed_matrix.cpp
+++ src/fixed_matrix.cpp
@@ -2,58 +2,58 @@
 
 template <typename T, int R, int C>
 class fixed_matrix {
-public:
+ public:
     fixed_matrix(std::initializer_list<std::initializer_list<T>> matrix) {
-        for(auto row: matrix) {
-            for(auto row_mat: mat_) {
+        for (auto row : matrix) {
+            for (auto row_mat : mat_) {
                 std::copy(row.begin(), row.end(), row_mat);
             }
         }
-       // std::copy(matrix.begin()->begin(), matrix.end()->end(), &mat_[0][0]);
     }
-    T const & operator()(int r, int c) const { 
+    T const & operator()(int r, int c) const {
         return mat_[r][c];
     }
-private:
+ private:
     T mat_[R][C];
 };
 
 template <typename T, int R, int C>
-void print(const fixed_matrix<T,R,C> & m) {
-    for (int i=0;i<R;++i) { 
-        for (int j=0;j<C;++j) {
-            std::cout << m(i,j) << " "; 
+void print(const fixed_matrix<T, R, C> & m) {
+    for (int i = 0; i < R; ++i) {
+        for (int j = 0; j < C; ++j) {
+            std::cout << m(i, j) << " ";
         }
-        std::cout << "\n"; 
+        std::cout << "\n";
     }
 }
 
 void test1() {
-    fixed_matrix<double,3,4> a{{1,1,2}, {2,2,2} }; std::cout << "matrix 1\n";
+    fixed_matrix<double, 3, 4> a{{1, 1, 2}, {2, 2, 2} };
+    std::cout << "matrix 1\n";
     print(a);
     std::cout << std::endl;
 }
 
 void test2() {
-    fixed_matrix<double,3,4> a{{1,1,2}, {2,2,2}, {1}, }; std::cout << "matrix 2\n";
+    fixed_matrix<double, 3, 4> a{{1, 1, 2}, {2, 2, 2}, {1}, };
+    std::cout << "matrix 2\n";
     print(a);
     std::cout << std::endl;
 }
 
-void test3() { 
+void test3() {
     try {
-        fixed_matrix<double, 3, 4> a{{1, 1, 2}, {2, 2, 2},{1},{-1}};
+        fixed_matrix<double, 3, 4> a{{1, 1, 2}, {2, 2, 2}, {1}, {-1}};
         std::cout << "matrix 3\n";
         print(a);
-        std::cout << std::endl; 
+        std::cout << std::endl;
     }
     catch(...) {
         std::cerr << "cannot initialize matrix\n";
-    } 
+    }
 }
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
     test1();
     test2();
     test3();
